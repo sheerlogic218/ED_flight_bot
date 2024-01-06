b9969f81b857b11a681cd988607142a1d6cd94be
Bailen O'Farrell <59749194+sheerlogic218@users.noreply.github.com>
2024-01-06 15:09:49 +0000

    edits

--- first_test.py
+++ first_test.py
@@ -18,25 +18,25 @@ def screen_shot():
     #takes a screenshot
     myScreenshot = pyautogui.screenshot()
     #saves the screenshot
-    myScreenshot.save(r'elite_dangerous_bot/screen_shot.png')
+    myScreenshot.save(r'pictures/screen_shot.png')
     return myScreenshot
 
 #converts the screenshot to mono color, with infinite contrast
 def mono_color():
-    image = Image.open('elite_dangerous_bot/screen_shot.png')
+    image = Image.open('pictures/screen_shot.png')
     #converts the screenshot to grayscale
     image = image.convert('L')
     #saves grayscale screenshot
-    image.save(r'elite_dangerous_bot/gray_scale.png')
+    image.save(r'pictures/gray_scale.png')
     #converts the screenshot to black and white
     image = image.point(lambda x: 0 if x < 100 else 255, '1')
     #saves the screenshot
-    image.save(r'elite_dangerous_bot/mono_color.png')
+    image.save(r'pictures/mono_color.png')
     return image
 
 #finds where the compass is on screen, and returns the coordinates of the compass
 def find_compass():
-    compass = Image.open('elite_dangerous_bot/compass_sample.png')
+    compass = Image.open('pictures/compass_sample.png')
     compass_location = pyautogui.locateCenterOnScreen(compass, grayscale=True, confidence=0.7)
     if compass_location != None:
         print(compass_location)
@@ -44,7 +44,7 @@ def find_compass():
         bounds = 45
         box = (compass_location[0]-bounds, compass_location[1]-bounds, compass_location[0]+bounds, compass_location[1]+bounds)
         #saves the screenshot
-        screen_shot().crop(box).save(r'elite_dangerous_bot/found_compass.png')
+        screen_shot().crop(box).save(r'pictures/found_compass.png')
 
 
     else:
