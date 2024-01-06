dd44b3120634cd6d2229554ab871643042032329
Bailen O'Farrell <59749194+sheerlogic218@users.noreply.github.com>
2024-01-06 23:23:51 +0000

    required input logic is implemented

--- first_test.py
+++ first_test.py
@@ -16,22 +16,16 @@ from PIL import ImageChops
 
 #takes a screenshot and saves it to elite_dangerous_bot
 def screen_shot():
-    #takes a screenshot
     myScreenshot = pyautogui.screenshot()
-    #saves the screenshot
     myScreenshot.save(r'pictures/screen_shot.png')
     return myScreenshot
 
-#converts the screenshot to mono color, with infinite contrast
+#converts the screenshot to mono color
 def mono_color():
     image = Image.open('pictures/screen_shot.png')
-    #converts the screenshot to grayscale
     image = image.convert('L')
-    #saves grayscale screenshot
     image.save(r'pictures/gray_scale.png')
-    #converts the screenshot to black and white
     image = image.point(lambda x: 0 if x < 100 else 255, '1')
-    #saves the screenshot
     image.save(r'pictures/mono_color.png')
     return image
 
@@ -41,10 +35,8 @@ def find_compass():
     compass_location = pyautogui.locateCenterOnScreen(compass, grayscale=True, confidence=0.7)
     if compass_location != None:
         print(compass_location)
-        #draws a box around the found compass
         bounds = 35
         box = (compass_location[0]-bounds, compass_location[1]-bounds, compass_location[0]+bounds, compass_location[1]+bounds)
-        #saves the screenshot
         screen_shot().crop(box).save(r'pictures/found_compass.png')
     else:
         print('compass not found')
@@ -62,11 +54,30 @@ def target_deviation():
     brightest = cv2.minMaxLoc(solid_difference)
     coords = brightest[3]
     deviation = (coords[0]-35, coords[1]-35)
-    print(coords)
-    print(deviation) 
+    return deviation
+    
 
-
-
+def output(deviation):
+    if abs(deviation[0]) < 4 and abs(deviation[1]) > 4:
+        #target is horizontally centered
+        if deviation[1] > 4:
+            #target is above
+            print('target is below')
+        elif deviation[1] < -4:
+            #target is below
+            print('target is above')
+    elif abs(deviation[0]) > 4:
+        #target is not horizontally centered
+        if deviation[0] > 4:
+            #target is to the right
+            print('target is to the right')
+        elif deviation[0] < -4:
+            #target is to the left
+            print('target is to the left')
+    elif abs(deviation[0]) < 4 and abs(deviation[1]) < 4:
+        #target is centered
+        print('target is centered')
+    print(deviation)
 
 
 
@@ -77,4 +88,5 @@ while True:
     #screen_shot()
     #mono_color()
     find_compass()
-    target_deviation()
+    dev = target_deviation()
+    output(dev)
